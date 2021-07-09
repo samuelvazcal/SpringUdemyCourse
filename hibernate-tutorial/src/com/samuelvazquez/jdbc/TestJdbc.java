e205096756aafd68f450af4d92393f9bdbae38e6
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-07-08 19:50:03 -0500

    setting up of Hibernate and JDBC

---
+++ hibernate-tutorial/src/com/samuelvazquez/jdbc/TestJdbc.java
@@ -0,0 +1,22 @@
+package com.samuelvazquez.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+    public static void main(String[] args) {
+        String dbName = "hb_student_tracker";
+        String jdbcUrl = "jdbc:mysql://localhost/"+dbName;
+        String user = "hbstudent";
+        String pass = "hbstudent";
+
+        try {
+            System.out.println("Connecting to database: " + jdbcUrl);
+            Connection myConn = DriverManager.getConnection(jdbcUrl,user,pass);
+
+            System.out.println("Connection successful!!!");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
