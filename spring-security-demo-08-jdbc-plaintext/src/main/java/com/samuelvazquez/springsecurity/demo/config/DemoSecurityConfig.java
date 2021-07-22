d2bc7c460e9648dda1a530c5db430b9a0f44b9ac
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-07-22 17:46:00 -0500

    added Spring Security implementation using DataSource bean and a Environment instance

--- spring-security-demo-08-jdbc-plaintext/src/main/java/com/samuelvazquez/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-08-jdbc-plaintext/src/main/java/com/samuelvazquez/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.samuelvazquez.springsecurity.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,18 +8,30 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // add a reference to our security data source
+    @Autowired
+    private DataSource securityDataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        // add our users for in memory authentication
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-                .withUser(users.username("mary").password("test123").roles("EMPLOYEE","MANAGER"))
-                .withUser(users.username("susan").password("test123").roles("EMPLOYEE","ADMIN"));
+//        // add our users for in memory authentication
+//        User.UserBuilder users = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+//                .withUser(users.username("mary").password("test123").roles("EMPLOYEE","MANAGER"))
+//                .withUser(users.username("susan").password("test123").roles("EMPLOYEE","ADMIN"));
+
+        // no more hardcoded users, now using jdbc authentication | jdbcAuthentication tells Spring Security to use
+        // JDBC authentication with our data source | DataSource points to those database tables (users & authorities)
+        auth.jdbcAuthentication().dataSource(securityDataSource);
+
     }
 
     // allows configure security of web paths in application, login, logout, etc
