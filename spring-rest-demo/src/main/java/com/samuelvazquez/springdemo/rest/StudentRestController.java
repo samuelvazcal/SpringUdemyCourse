d63e7733260861f5efef11405344032e193cb527
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-07-25 15:12:31 -0500

    added exception handling for bad input

--- spring-rest-demo/src/main/java/com/samuelvazquez/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/samuelvazquez/springdemo/rest/StudentRestController.java
@@ -1,6 +1,8 @@
 package com.samuelvazquez.springdemo.rest;
 
 import com.samuelvazquez.springdemo.entity.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
@@ -37,6 +39,41 @@ public class StudentRestController {
 
         // just index into the list ... keep it simple for now
 
+        //check the studentId against list size
+        if((studentId >= theStudents.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
         return theStudents.get(studentId);
     }
+
+    // add an exception handler using @ExceptionHandler
+    // this is an exception handler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e) {
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(e.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+        // error, body of the response |
+        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
+
+    // add another exception handler --- to catch any exception (catch all)
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception e) {
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        //error.setMessage(e.getMessage());
+        error.setMessage("you typed bad input");
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+    }
+
 }
