e852151c5a0dcc2f2e80db80add3621db57c9c4b
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-07-25 11:16:10 -0500

    added PathVariable support for REST endpoints

--- spring-rest-demo/src/main/java/com/samuelvazquez/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/samuelvazquez/springdemo/rest/StudentRestController.java
@@ -1,10 +1,9 @@
 package com.samuelvazquez.springdemo.rest;
 
 import com.samuelvazquez.springdemo.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,16 +11,32 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    private List<Student> theStudents;
+
+    // define @PostConstruct to load the student data --- only once!
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Michael","Scott"));
+        theStudents.add(new Student("Dwight","Schrute"));
+        theStudents.add(new Student("Jim","Halpert"));
+        theStudents.add(new Student("Pam","Beesly"));
+        theStudents.add(new Student("Stanley","Hudson"));
+    }
+
     // define endpoint for "/students" - return list of students
 
     @GetMapping("/students")
     public List<Student> getStudents() {
-        List<Student> studentList = new ArrayList<>();
-        studentList.add(new Student("Michael","Scott"));
-        studentList.add(new Student("Dwight","Schrute"));
-        studentList.add(new Student("Jim","Halpert"));
-        studentList.add(new Student("Pam","Beesly"));
-        studentList.add(new Student("Stanley","Hudson"));
-        return studentList;
+        return theStudents;
+    }
+
+    // define endpoint for "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // just index into the list ... keep it simple for now
+
+        return theStudents.get(studentId);
     }
 }
