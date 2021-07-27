cd187d35bab5721db2cc75a853219c96354f6de6
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-07-26 19:11:10 -0500

    added exception handling for GET operations

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/config/DemoAppConfig.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/config/DemoAppConfig.java
@@ -35,8 +35,7 @@ public class DemoAppConfig implements WebMvcConfigurer {
 	private Environment env;
 	
 	private Logger logger = Logger.getLogger(getClass().getName());
-	
-	// define a bean for ViewResolver
+
 
 	@Bean
 	public DataSource myDataSource() {
@@ -46,7 +45,7 @@ public class DemoAppConfig implements WebMvcConfigurer {
 
 		// set the jdbc driver
 		try {
-			myDataSource.setDriverClass("com.mysql.jdbc.Driver");		
+			myDataSource.setDriverClass("com.mysql.jdbc.Driver");
 		}
 		catch (PropertyVetoException exc) {
 			throw new RuntimeException(exc);
