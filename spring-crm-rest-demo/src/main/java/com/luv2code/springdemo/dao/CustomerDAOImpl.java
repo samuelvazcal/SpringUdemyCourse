357bb8320a173364b7179ce93ae1ed9d883196f1
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-07-29 11:26:54 -0500

    removal of white spaces

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -65,7 +65,7 @@ public class CustomerDAOImpl implements CustomerDAO {
 		Session currentSession = sessionFactory.getCurrentSession();
 		
 		// delete object with primary key
-		Que ry theQuery =
+		Query theQuery =
 				currentSession.createQuery("delete from Customer where id=:customerId");
 		theQuery.setParameter("customerId", theId);
 		
