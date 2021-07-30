eff1cf48136c5afc3d371366d4b7bb19aae15b4a
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-07-30 18:01:15 -0500

    added delete functionality for Spring Boot MVC application

--- thymeleafdemo-employees-list-db/src/main/java/com/samuelvazquez/springboot/thymeleafdemo/controller/EmployeeController.java
+++ thymeleafdemo-employees-list-db/src/main/java/com/samuelvazquez/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -57,4 +57,14 @@ public class EmployeeController {
         // use a redirect to prevent duplicate submissions POST/Redirect/GET pattern
         return "redirect:/employees/list";
     }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId) {
+
+        // delete the employee
+        employeeService.deleteById(theId);
+
+        // redirect to /employees/list
+        return "redirect:/employees/list";
+    }
 }
