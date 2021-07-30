967c30c6c6b0a77444d9ffc53a21e04a0b7b6580
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-07-30 17:52:45 -0500

    added update operation for the MVC application

--- thymeleafdemo-employees-list-db/src/main/java/com/samuelvazquez/springboot/thymeleafdemo/controller/EmployeeController.java
+++ thymeleafdemo-employees-list-db/src/main/java/com/samuelvazquez/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.samuelvazquez.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,6 +37,16 @@ public class EmployeeController {
         // create model attribute to bind form data
         Employee theEmployee = new Employee();
         theModel.addAttribute("employee",theEmployee);
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+        // get the employee from the service
+        Employee theEmployee = employeeService.findById(theId);
+        //set employee as a model attribute to pre-populate the form
+        theModel.addAttribute("employee",theEmployee);
+        //send over to our form
         return "employees/employee-form";
     }
 
