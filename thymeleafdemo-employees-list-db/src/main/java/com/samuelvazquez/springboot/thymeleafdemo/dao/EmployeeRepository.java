5b7077451e49b70ad562a21f5cf89b627083d694
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-07-30 15:57:57 -0500

    added Add Employee functionality

--- thymeleafdemo-employees-list-db/src/main/java/com/samuelvazquez/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ thymeleafdemo-employees-list-db/src/main/java/com/samuelvazquez/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -3,9 +3,15 @@ package com.samuelvazquez.springboot.thymeleafdemo.dao;
 import com.samuelvazquez.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code
+
+    // add a method to sort by last name
+    List<Employee> findAllByOrderByLastNameAsc();
+
 	
 }
