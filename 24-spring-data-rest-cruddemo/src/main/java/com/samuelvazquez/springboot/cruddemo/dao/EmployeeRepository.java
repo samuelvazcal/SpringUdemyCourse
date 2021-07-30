e41f36fd525154d28fdd4e483bb357d04ad3d9ae
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-07-29 19:24:53 -0500

    added configuration for pagination and sorting thanks to Spring Data REST

--- 24-spring-data-rest-cruddemo/src/main/java/com/samuelvazquez/springboot/cruddemo/dao/EmployeeRepository.java
+++ 24-spring-data-rest-cruddemo/src/main/java/com/samuelvazquez/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.samuelvazquez.springboot.cruddemo.dao;
 
 import com.samuelvazquez.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
 }
